0c05131c6feb5a2447d4330ac92dfba7376026aa
Matt Post <post@cs.jhu.edu>
2020-02-17 16:48:55 -0600

    prints all labels correctly

--- momento.py
+++ momento.py
@@ -15,29 +15,22 @@ from typing import List
 
 template_header = r"""\documentclass{article}
 
-\usepackage[legalpaper, total={8in, 16in}]{geometry}
+\usepackage[letterpaper, total={6.5in, 10in}]{geometry}
 \usepackage{adjustbox}
 \usepackage{graphicx}
 \usepackage{pdfpages}
 \usepackage{booktabs}
 \usepackage{colortbl}
 
+\pagestyle{empty}
+
 \begin{document}
 
 \begin{table}[p]
   \begin{adjustbox}{max width=\textwidth}
-  \begin{tabular}{|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|}
+  \begin{tabular}{|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c}
 """
-
-template_row = r"""
-    \cline{11-52}
-    \multicolumn{10}{c|}{} & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & \\
-    \hline
-    & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & \\
-    \hline
-    & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & \\
-    \hline
-"""
+#   \multicolumn{53}{c}{\LARGE MOMENTO MORI\newline} \\
 
 template_footer = r"""
   \end{tabular}
@@ -50,40 +43,46 @@ template_footer = r"""
 default_color = "white"
 
 
-def build_cells(color=default_color, num=52, label="", lastlabel="") -> List[str]:
+def build_cells(color=default_color, num=52, label_until=52) -> List[str]:
     cells = []
     for weekno in range(1, num + 1):
-        label = lastlabel if weekno == num else label
+        label = "x" if weekno <= label_until else ""
         cells.append(fr"\cellcolor{{{color}}}{label}")
     return cells
 
 def main(args):
-    current_week = datetime.utcnow().strftime("%U")
+    current_week = int(datetime.utcnow().strftime("%U"))
 
     table = ""
     color = "white"
     print(template_header, file=args.outfile)
     print(fr"  \cline{{{args.birth_week+1}-52}}", file=args.outfile)
     print(fr"\multicolumn{{{args.birth_week}}}{{c|}}{{}} & ", file=args.outfile)
-    print(" & ".join(build_cells(color=color, num=52-args.birth_week)), file=args.outfile)
-    print(r"\\")
-    print(r"\hline")
+    print(" & ".join(build_cells(color=color, num=52-args.birth_week, label_until=52)), r" \\", file=args.outfile)
+    print(fr"  \cline{{1-52}}", file=args.outfile)
 
     for year in range(2, args.years):
         if year % 10 == 0:
             color = "gray!25"
-            lastlabel = str(year)
+            finalcol = rf" & {year} \\"
         else:
             color = "white"
-            lastlabel = ""
+            finalcol = r" \\"
 
         if year <= args.age:
             label = "x"
         else:
             label = ""
 
-        print(" & ".join(build_cells(color=color, label=label, lastlabel=lastlabel)), r"\\", file=args.outfile)
-        print(r"  \hline", file=args.outfile)
+        if year > args.age + 1:
+            label_until = 0
+        elif year == args.age + 1:
+            label_until = current_week
+        else:
+            label_until = 52
+
+        print(" & ".join(build_cells(color=color, label_until=label_until)), finalcol, file=args.outfile)
+        print(fr"  \cline{{1-52}}", file=args.outfile)
 
     print(template_footer, file=args.outfile)
 
