9394b7bb5b46780b4de1c5486f4a7f062ce4f4cf
Matt Post <post@cs.jhu.edu>
2020-02-17 12:06:10 -0600

    Initial commit with working code

---
+++ momento.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import sys
+from datetime import datetime
+from typing import List
+
+template_header = r"""\documentclass{article}
+
+\usepackage[legalpaper, total={8in, 16in}]{geometry}
+\usepackage{adjustbox}
+\usepackage{graphicx}
+\usepackage{pdfpages}
+\usepackage{booktabs}
+\usepackage{colortbl}
+
+\begin{document}
+
+\begin{table}[p]
+  \begin{adjustbox}{max width=\textwidth}
+  \begin{tabular}{|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|c|}
+"""
+
+template_row = r"""
+    \cline{11-52}
+    \multicolumn{10}{c|}{} & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & \\
+    \hline
+    & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & \\
+    \hline
+    & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & & \\
+    \hline
+"""
+
+template_footer = r"""
+  \end{tabular}
+  \end{adjustbox}
+\end{table}
+
+\end{document}
+"""
+
+default_color = "white"
+
+
+def build_cells(color=default_color, num=52, lastlabel="") -> List[str]:
+    cells = []
+    for weekno in range(1, num + 1):
+        label = lastlabel if weekno == num else ""
+        cells.append(fr"\cellcolor{{{color}}}\footnotesize{{{label}}}")
+    return cells
+
+def main(args):
+    current_week = datetime.utcnow().strftime("%U")
+
+    table = ""
+    color = "gray!25"
+    print(template_header, file=args.outfile)
+    print(fr"  \cline{{{args.birth_week}-52}}", file=args.outfile)
+    print(" & ".join(build_cells(color=color, num=52-args.birth_week+1)), file=args.outfile)
+    print(r"\\")
+    print(r"\hline")
+
+    for year in range(2, args.years):
+        if year % 10 == 0:
+            color = "gray!25"
+            label = f"{year}"
+        else:
+            color = "white"
+            label = ""
+
+        print(" & ".join(build_cells(color=color, lastlabel=label)), r"\\", file=args.outfile)
+        print(r"  \hline", file=args.outfile)
+
+    print(template_footer, file=args.outfile)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--birth-week", "-b" , type=int, default=1,
+                        help="Week number of birthday (1--52)")
+    parser.add_argument("--age", "-a", type=int, default=0,
+                        help="Your current age")
+    parser.add_argument("--years", "-y", type=int, default=78,
+                        help="Years you expect to live")
+    parser.add_argument("--title", "-t", type=str, default="momento mori",
+                        help="The document title")
+    parser.add_argument("--outfile", "-o", type=argparse.FileType("w"), default=sys.stdout,
+                        help="File to write to")
+    args = parser.parse_args()
+
+    main(args)
